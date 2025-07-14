74ab5cd32c963d8cf01dff434885f3d286abe978
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-07-14 22:02:54 +0300

    fix

--- app/postcss.config.mjs
+++ app/postcss.config.mjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // ✅ بدلاً من tailwindcss
+    tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // لو بتستخدم app router
-    "./pages/**/*.{js,ts,jsx,tsx}", // لو بتستخدم pages router
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
